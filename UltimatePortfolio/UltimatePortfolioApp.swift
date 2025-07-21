6e83ba9799d18d2c96173600309b86410849b48b
Henrik Jones <henrikjones22@gmail.com>
2025-07-21 11:35:56 -0400

    starting this project out

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Henrik Jones on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
